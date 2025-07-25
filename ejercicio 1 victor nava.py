2f5c74b44c4b6a1c252e97f5f3ef26072c88d8c7
Victor Nava <122217022+XenoJiva04@users.noreply.github.com>
2025-07-24 22:43:01 -0400

    Add files via upload

--- ejercicio 1 victor nava.py
+++ ejercicio 1 victor nava.py
@@ -3,16 +3,14 @@
 """contador de vocales y consonantes"""
 #objetivo: practicar bucles for y condicionales if-elif y else con cadenas de texto
 
-print("saludos amigo, necesito una frase para mi slogan, me dirias una?")
-hola = input ("cual es tu frase?")
-if hola == "perro que ladra no muerde":
-    print("que buena frase amigo")
-elif hola == "mordisco salsa":
-    print("acaso tienes hambre?")
-else:
-    print("mejor no digas nada")
-
-for i in range(9):
-    print(f"iteracion {hola}")
-
-type lower(hola)
+print("\n--- Ejercicio 1: Contador de Vocales y Consonantes ---")
+frase = input("Ingresa una frase: ")
+vocales = 0
+consonantes = 0
+vocales_lista = "aeiouáéíóú"
+for letra in frase.lower():
+  if letra in vocales_lista:
+     vocales += 1
+  elif letra.isalpha():
+     consonantes += 1
+print(f"La frase '{frase}' tiene {vocales} vocales y {consonantes} consonantes.")
