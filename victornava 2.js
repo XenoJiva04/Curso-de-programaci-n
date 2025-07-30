cd5907c29f0decd4d60459196af521158838a91d
Victor Nava <122217022+XenoJiva04@users.noreply.github.com>
2025-07-30 12:36:28 -0400

    Add files via upload

---
+++ victornava 2.js
@@ -0,0 +1,6 @@
+//calcula el area de un rectangulo
+
+let a = 5;
+let b = 4;
+let multiplicacion = a * b;
+console.log(`multiplicacion: ${multiplicacion}`)
---
+++ victornava.js
@@ -0,0 +1,6 @@
+let nombre = "Victor";
+console.log("nombre de usuario:", nombre);
+const fechaDeNacimiento = "04/08/2001";
+console.log("registrado en:", fechaDeNacimiento);
+let ciudadDeOrigen = "San Francisco";
+console.log("nacido en:", ciudadDeOrigen);
